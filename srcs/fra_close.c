dbeb22c75194621548f8c5e5f7f0cfbc3c3fbf61
Eric LI <erli@student.42.fr>
2018-12-14 10:32:39 +0100

    protection des closes si param == 0

--- srcs/fra_close.c
+++ srcs/fra_close.c
@@ -6,7 +6,7 @@
 /*   By: erli <erli@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 10:50:30 by erli              #+#    #+#             */
-/*   Updated: 2018/12/12 14:18:24 by erli             ###   ########.fr       */
+/*   Updated: 2018/12/14 10:31:44 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	fra_close(void *param)
 {
 	t_fra_param	*para;
 
+	if (param == 0)
+		return (0);
 	para = (t_fra_param *)param;
 	fra_free_param(&para, 0, 11112);
 	exit(0);
+	return (0);
 }
--- srcs/fra_key_close.c
+++ srcs/fra_key_close.c
@@ -6,7 +6,7 @@
 /*   By: erli <erli@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 11:44:00 by erli              #+#    #+#             */
-/*   Updated: 2018/12/12 14:04:23 by erli             ###   ########.fr       */
+/*   Updated: 2018/12/14 10:32:08 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		fra_key_close(int key, void *arg)
 {
 	t_fra_param *param;
 
+	if (param == 0)
+		return (0);
 	param = (t_fra_param *)arg;
 	if (key == 53)
 	{
